535af12c5cd23a478096ee9e2f55cfb20376aa8c
Vishalanshuman <vishalanshuman@gmail.com>
2023-02-04 21:33:55 +0530

    update

--- main/form.py
+++ main/form.py
@@ -5,7 +5,7 @@ class EmployeeForm(forms.ModelForm):
     class Meta:
         model = employee_store
         fields = ("candidate", "education", "employment", "aadhar", "pan", "passport", "address", "employment_file", "education_file", "passport_file", "aadhar_file", "form16_file")
-        labels = {'passport': 'Passport Number', 'address': 'Present Address From To', 'form16_file': 'Form 16', 'aadhar_file': 'Aadhar Number', 'employment_file': 'Employment', 'education_file': 'Education'}
+        labels = {'passport': 'Passport Number', 'address': 'Present Address From To', 'form16_file': 'Form 16', 'aadhar_file': 'Aadhar File', 'employment_file': 'Employment', 'education_file': 'Education'}
         widgets = {
             "education" : forms.TextInput(attrs={'class': 'form-control'}),
             "employment" : forms.TextInput(attrs={'class': 'form-control'}),
@@ -33,7 +33,17 @@ class EmployeeForm(forms.ModelForm):
             for f in self.request.FILES.getlist('file'):
                 obj = self.model.objects.create(file=f)
         return super(EmployeeForm, self).form_valid(form)
-
+    
+    def clean_pan(self):
+        pan = self.cleaned_data['pan']
+        if len(pan)<9:
+            raise forms.ValidationError("Pan Number must contain 10 digits.")
+        return pan
+    def clean_aadhar(self):
+        aadhar = self.cleaned_data['aadhar']
+        if len(aadhar)<11 or  not aadhar.isdigit():
+            raise forms.ValidationError("Adhar Number must contain 12 digits and integer input")
+        return aadhar
 class VerificationForm(forms.ModelForm):
     class Meta:
         model = verification_store
