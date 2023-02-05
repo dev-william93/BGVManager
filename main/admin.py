a856b2042205622fd5226ccc115969f9a1e30934
afranzio <maniafranzio1234@gmail.com>
2023-02-05 09:43:08 +0530

    fixes

--- main/admin.py
+++ main/admin.py
@@ -3,4 +3,8 @@ from .models import *
 
 # Register your models here.
 admin.site.register(employee_store)
-admin.site.register(verification_store)
+
+
+class VerificationAdmin(admin.ModelAdmin):
+    list_display = ('candidate', 'overAll_status')
+admin.site.register(verification_store,VerificationAdmin)
